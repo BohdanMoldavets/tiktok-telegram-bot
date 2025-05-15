441cf7127a43b4e13b80a28faace7c09980deefd
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-05-15 15:25:01 +0200

    Initial commit

---
+++ src/main/java/com/moldavets/tiktok_telegram_bot/TiktokTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.moldavets.tiktok_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiktokTelegramBotApplication.class, args);
+	}
+
+}
