c18a14cb3b79bd9dcb8ea49149a34e461508cff1
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-05-26 13:34:14 +0200

    Add tests for UnknownCommand

---
+++ src/test/java/com/moldavets/tiktok_telegram_bot/command/Impl/UnknownCommandTest.java
@@ -0,0 +1,46 @@
+package com.moldavets.tiktok_telegram_bot.command.Impl;
+
+import com.moldavets.tiktok_telegram_bot.logger.Impl.TelegramCustomLogger;
+import com.moldavets.tiktok_telegram_bot.utils.MessageText;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+
+@ExtendWith(MockitoExtension.class)
+class UnknownCommandTest {
+
+    @InjectMocks
+    UnknownCommand unknownCommand;
+
+    @Test
+    void execute_shouldWithUnknownCommandText_whenInputContainsValidInputData() {
+        Update update = new Update();
+        Message message = new Message();
+        User user = new User();
+
+        user.setId(123L);
+        user.setUserName("tester");
+        message.setText("/test");
+        message.setFrom(user);
+        update.setMessage(message);
+
+        try(MockedStatic<TelegramCustomLogger> mockedLogger = Mockito.mockStatic(TelegramCustomLogger.class)) {
+            TelegramCustomLogger mockTelegramCustomLogger = Mockito.mock(TelegramCustomLogger.class);
+
+            mockedLogger.when(TelegramCustomLogger::getInstance).thenReturn(mockTelegramCustomLogger);
+
+            SendMessage actual = (SendMessage) unknownCommand.execute(update);
+
+            Assertions.assertEquals(user.getId().toString(), actual.getChatId());
+            Assertions.assertEquals(MessageText.COMMAND_UNKNOWN.getMessageText(), actual.getText());
+        }
+    }
+}
