bb2d9eb50d17239cf7d8f9570c46997e6bfd1796
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-05-26 14:09:45 +0200

    Add test for CommandContainer

---
+++ src/test/java/com/moldavets/tiktok_telegram_bot/command/CommandContainerTest.java
@@ -0,0 +1,57 @@
+package com.moldavets.tiktok_telegram_bot.command;
+
+import com.moldavets.tiktok_telegram_bot.command.Impl.AdministratorCommand;
+import com.moldavets.tiktok_telegram_bot.command.Impl.StartCommand;
+import com.moldavets.tiktok_telegram_bot.command.Impl.UnknownCommand;
+import com.moldavets.tiktok_telegram_bot.service.TelegramChannelService;
+import com.moldavets.tiktok_telegram_bot.service.TelegramUserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CommandContainerTest {
+
+    @Mock
+    TelegramUserService telegramUserService;
+
+    @Mock
+    TelegramChannelService telegramChannelService;
+
+    CommandContainer commandContainer;
+
+    @BeforeEach
+    void setUp() {
+        commandContainer = new CommandContainer(telegramUserService, telegramChannelService, 123L);
+    }
+
+    @Test
+    void retrieveCommand_shouldReturnStartCommand_whenInputContainsValidData() {
+        Command actual = commandContainer.retrieveCommand("/start");
+
+        assertNotNull(actual);
+        assertInstanceOf(StartCommand.class, actual);
+    }
+
+    @Test
+    void retrieveCommand_shouldReturnAdministratorCommand_whenInputContainsValidData() {
+        Command actual = commandContainer.retrieveCommand("/administrator");
+
+        assertNotNull(actual);
+        assertInstanceOf(AdministratorCommand.class, actual);
+    }
+
+    @Test
+    void retrieveCommand_shouldReturnUnknownCommand_whenInputContainsInvalidData() {
+        Command actual = commandContainer.retrieveCommand("/test");
+
+        assertNotNull(actual);
+        assertInstanceOf(UnknownCommand.class, actual);
+    }
+
+
+}
